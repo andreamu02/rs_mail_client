4cea786dec6cf23384340e034690a8ee370f652a
andreamu02 <andrea.mutti@studenti.unipd.it>
2025-12-19 14:49:18 +0100

    feat(TUI): add list control/delete

--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -1,5 +1,7 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::prelude::Stylize;
+use ratatui::style::Style;
+use ratatui::widgets::{self, ListState};
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, Event},
@@ -11,6 +13,7 @@ use ratatui::{
 #[derive(Debug, Default)]
 struct AppState {
     items: Vec<TodoItem>,
+    list_state: ListState,
 }
 
 #[derive(Debug, Default)]
@@ -48,6 +51,21 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
                 event::KeyCode::Esc => {
                     break;
                 }
+                event::KeyCode::Char(char) => match char {
+                    'd' => {
+                        if let Some(index) = app_state.list_state.selected() {
+                            app_state.items.remove(index);
+                        }
+                    }
+
+                    'j' => {
+                        app_state.list_state.select_next();
+                    }
+                    'k' => {
+                        app_state.list_state.select_previous();
+                    }
+                    _ => {}
+                },
 
                 _ => {}
             }
@@ -56,7 +74,7 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &AppState) {
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
         .areas(frame.area());
@@ -70,13 +88,16 @@ fn render(frame: &mut Frame, app_state: &AppState) {
         .fg(Color::Yellow)
         .render(border_area, frame.buffer_mut());
 
-    List::new(
+    let list = List::new(
         app_state
             .items
             .iter()
             .map(|x| ListItem::from(x.description.clone())),
     )
-    .render(inner_area, frame.buffer_mut());
+    .highlight_symbol(">")
+    .highlight_style(Style::default().fg(Color::Green));
+
+    frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
 
     // Paragraph::new("Hello from application").render(frame.area(), frame.buffer_mut());
 }
